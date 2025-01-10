f90535b6ea46e53990e64aedbe65b382c631ce68
Hien Nguyen <hien.qqnguyen@gmail.com>
2025-01-10 03:51:08 +0000

    WIP

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -9,17 +9,25 @@
 #include <stdio.h>
 #include <syscall-nr.h>
 
-typedef bool (*validate_func)(void* args);
+typedef bool (*validate_func)(struct intr_frame* f, void* args);
 
-typedef void (*handler_func)(void* args);
+typedef void (*handler_func)(struct intr_frame* f, void* args);
 
 static void syscall_handler(struct intr_frame*);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
-static bool is_valid_pointer(void* pointer) { return true; }
+/* Helper functions */
 
-static bool is_valid_buffer(void* buffer, size_t size) { return true; }
+static bool is_valid_pointer(void* pointer) { return true; }
+static bool is_valid_fd(void* pointer) { return true; }
+static bool is_valid_buffer(void* buffer, size_t size) {
+  /* First of all, this pointer needs to be in user space */
+
+  /* */
+
+  /* The data it points to must also */
+}
 
 // Array of syscall handlers
 static void terminate(struct intr_frame* f, int status) {
@@ -34,17 +42,42 @@ static bool validate_syscall_number(struct intr_frame* f, int syscall_number) {
     return true;
   }
 
+  printf("%s", "Invalid syscall number");
+
   terminate(f, -1);
 };
 
-static bool validate_exit(void* args) { return true; };
-static bool validate_exec(void* args) { return true; };
-static bool validate_create(void* args) { return true; };
-static bool validate_wait(void* args) { return true; };
-static bool validate_remove(void* args) { return true; };
-static bool validate_open(void* args) { return true; };
-static bool validate_read(void* args) { return true; };
-static bool validate_write(void* args) { return true; };
+/* Validation functions */
+static bool validate_exit(struct intr_frame* f, void* args) {
+  /* We need to validate the first arguments */
+  return true;
+};
+
+static bool validate_exec(struct intr_frame* f, void* args) { return true; };
+static bool validate_create(struct intr_frame* f, void* args) { return true; };
+static bool validate_wait(struct intr_frame* f, void* args) { return true; };
+static bool validate_remove(struct intr_frame* f, void* args) { return true; };
+static bool validate_open(struct intr_frame* f, void* args) { return true; };
+static bool validate_read(struct intr_frame* f, void* args) { return true; };
+
+static bool validate_write(struct intr_frame* f, void* args) {
+  /* int fd, const void* buffer, unsigned length */
+  uint32_t* syscall_arguments = (uint32_t*)args;
+
+  int fd = syscall_arguments[1];
+  void* buffer = syscall_arguments[2];
+  uint32_t size = syscall_arguments[3];
+
+  if (!is_valid_fd(fd)) {
+    return false;
+  };
+
+  if (!is_valid_buffer(buffer, size)) {
+    return false;
+  };
+
+  return true;
+};
 
 static validate_func syscall_validators[SYS_CALL_COUNT] = {
     [SYS_HALT] = NULL,
@@ -58,49 +91,59 @@ static validate_func syscall_validators[SYS_CALL_COUNT] = {
     [SYS_WRITE] = validate_write,
 };
 
-static void sys_halt_handler(void* args) {
+/* System call handlers */
+
+static void sys_halt_handler(struct intr_frame* f, void* args) { shutdown_power_off(); };
+
+static void sys_exec_handler(struct intr_frame* f, void* args) {
   printf("Executing a new process.\n");
   // Logic for exec syscall
 };
 
-static void sys_exec_handler(void* args) {
+static void sys_wait_handler(struct intr_frame* f, void* args) {
   printf("Executing a new process.\n");
   // Logic for exec syscall
 };
 
-static void sys_wait_handler(void* args) {
+static void sys_create_handler(struct intr_frame* f, void* args) {
   printf("Executing a new process.\n");
   // Logic for exec syscall
 };
 
-static void sys_create_handler(void* args) {
+static void sys_remove_handler(struct intr_frame* f, void* args) {
   printf("Executing a new process.\n");
   // Logic for exec syscall
 };
 
-static void sys_remove_handler(void* args) {
+static void sys_open_handler(struct intr_frame* f, void* args) {
   printf("Executing a new process.\n");
   // Logic for exec syscall
 };
 
-static void sys_open_handler(void* args) {
+static void sys_read_handler(struct intr_frame* f, void* args) {
   printf("Executing a new process.\n");
   // Logic for exec syscall
 };
 
-static void sys_read_handler(void* args) {
-  printf("Executing a new process.\n");
-  // Logic for exec syscall
+static void sys_write_handler(struct intr_frame* f, void* args) {
+  uint32_t* syscall_arguments = (uint32_t*)args;
+
+  int fd = syscall_arguments[1];
+  const void* buffer = (void*)syscall_arguments[2];
+  unsigned size = syscall_arguments[3];
+
+  if (fd == STDOUT_FILENO) {
+    putbuf(buffer, size);
+    f->eax = size; // Return the number of bytes written
+  } else {
+    // Handle other file descriptors (e.g., actual files)
+    f->eax = -1; // Not implemented yet
+  }
 };
 
-static void sys_write_handler(void* args) {
-  printf("Executing a new process.\n");
-  // Logic for exec syscall
-};
-
-static void sys_exit_handler(void* args) {
-  printf("Executing a new process.\n");
-  // Logic for exec syscall
+static void sys_exit_handler(struct intr_frame* f, void* args) {
+  uint32_t* syscall_arguments = (uint32_t*)args;
+  terminate(f, syscall_arguments[1]);
 };
 
 static handler_func syscall_handlers[SYS_CALL_COUNT] = {
@@ -111,23 +154,18 @@ static handler_func syscall_handlers[SYS_CALL_COUNT] = {
     [SYS_WRITE] = sys_write_handler,
 };
 
-static void dispatch_syscall(int syscall_number, void* args) {
-  if (syscall_handlers[syscall_number] != NULL) {
-    syscall_handlers[syscall_number](args);
-  } else {
-    printf("Syscall %d not implemented.\n", syscall_number);
-  }
-}
-
 static void syscall_handler(struct intr_frame* f) {
   uint32_t* args = ((uint32_t*)f->esp);
 
+  /* We assume that args[0] will always be available for us */
   uint32_t syscall_number = args[0];
 
+  /* Make sure the syscall_number is valid */
   validate_syscall_number(f, syscall_number);
 
+  /* Look up validator table based on syscall_number */
   if (syscall_validators[syscall_number] != NULL) {
-    bool is_valid = syscall_validators[syscall_number](args);
+    bool is_valid = syscall_validators[syscall_number](f, args);
     if (!is_valid) {
       // Handle invalid syscall arguments (e.g., terminate process, return error)
       printf("Syscall %d validation failed!\n", syscall_number);
@@ -135,5 +173,10 @@ static void syscall_handler(struct intr_frame* f) {
     }
   }
 
-  dispatch_syscall(syscall_number, args);
+  /* After validation, we call the system call handler */
+  if (syscall_handlers[syscall_number] != NULL) {
+    syscall_handlers[syscall_number](f, args);
+  } else {
+    printf("Syscall %d not implemented.\n", syscall_number);
+  }
 }

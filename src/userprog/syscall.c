f1392cf70fee9f1b86785834cb9090440c56a181
Hien Nguyen <hien.qqnguyen@gmail.com>
2025-01-12 01:09:28 +0000

    Finished set up successfully

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,173 +1,147 @@
 #include "userprog/syscall.h"
-#include <stdio.h>
-#include <syscall-nr.h>
+#include "userprog/pagedir.h"
+#include "userprog/process.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include "userprog/process.h"
 #include "devices/shutdown.h"
 #include "threads/vaddr.h"
+#include <stdio.h>
+#include <syscall-nr.h>
 #include <string.h>
+
+typedef bool (*validate_func)(struct intr_frame* f, uint32_t* args);
+typedef void (*handler_func)(struct intr_frame* f, uint32_t* args);
 
 static void syscall_handler(struct intr_frame*);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
+/* Helper functions */
 static void terminate(struct intr_frame* f, int status) {
   f->eax = status;
   printf("%s: exit(%d)\n", thread_current()->pcb->process_name, status);
   process_exit();
 }
 
-static int get_arg_count(uint32_t syscall_number) {
-  switch (syscall_number) {
-    case SYS_EXIT: // System call requires 1 argument
-      return 1;
-
-    case SYS_WRITE: // System call requires 3 arguments
-      return 3;
-
-    case SYS_EXEC: // System call requires 3 arguments
-      return 1;
-
-    case SYS_READ: // System call requires 3 arguments
-      return 3;
-
-    case SYS_OPEN: // System call requires 1 argument
-      return 1;
-
-    case SYS_CREATE: // System call requires 2 arguments
-      return 2;
-
-    default:     // Default case for unsupported syscalls
-      return -1; // Return -1 for invalid syscall number
-  }
-}
-
-static bool validate_pointer(void* pointer, size_t size) {
-  // Check if the base pointer is a valid user address
-  if (!is_user_vaddr(pointer)) {
+static bool is_valid_pointer(void* pointer, size_t size) {
+  if (!is_user_vaddr(pointer) || !is_user_vaddr((char*)pointer + size) ||
+      pagedir_get_page(thread_current()->pcb->pagedir, pointer) == NULL ||
+      pagedir_get_page(thread_current()->pcb->pagedir, (char*)pointer + size) == NULL) {
     return false;
   }
-
-  // Check if the end of the range is a valid user address
-  if (!is_user_vaddr(pointer + size)) {
-    return false;
-  }
-
-  // Check if the base pointer is mapped
-  if (!pagedir_get_page(thread_current()->pcb->pagedir, pointer)) {
-    return false;
-  }
-
-  // Check if the end of the range is mapped
-  if (!pagedir_get_page(thread_current()->pcb->pagedir, pointer + size)) {
-    return false;
-  }
-
   return true;
 }
 
-bool validate_buffer(void* pointer, size_t size) {
-  ASSERT(validate_pointer(pointer, 0) == true);
-
-  for (int i = 0; i <= size; i++) {
-    if (!pagedir_get_page(thread_current()->pcb->pagedir, (char*)(pointer + i))) {
+static bool is_valid_file(const char* file) {
+  while (*file != '\0') {
+    if (!is_user_vaddr(file) || pagedir_get_page(thread_current()->pcb->pagedir, file) == NULL) {
       return false;
     }
+    file++;
   }
-
   return true;
 }
 
-bool validate_file(char* file) {
-  int i = 0;
-  while (true) {
-    const char* current_address = file + i;
-
-    // Validate that the current address is a valid user address
-    if (!is_user_vaddr(current_address)) {
+static bool is_valid_buffer(void* buffer, size_t size) {
+  char* start = (char*)buffer;
+  char* end = start + size;
+  for (char* addr = start; addr < end; addr += PGSIZE) {
+    if (!pagedir_get_page(thread_current()->pcb->pagedir, addr)) {
       return false;
     }
-
-    // Validate that the current address is mapped
-    if (!pagedir_get_page(thread_current()->pcb->pagedir, current_address)) {
-      return false;
-    }
-
-    // Dereference the content at the current address
-    char content = *current_address;
-
-    // Check for null terminator
-    if (content == '\0') {
-      break;
-    }
-
-    i++;
   }
-
   return true;
 }
 
+/* Validation functions */
+static bool validate_halt(struct intr_frame* f UNUSED, uint32_t* args UNUSED) {
+  return true; // No arguments to validate
+}
+
+static bool validate_practice(struct intr_frame* f, uint32_t* args) {
+  return is_valid_pointer(&args[1], sizeof(uint32_t)); // Validate args[1]
+}
+
+static bool validate_exit(struct intr_frame* f, uint32_t* args) {
+  return true; // No validation needed for `SYS_EXIT`
+}
+
+static bool validate_exec(struct intr_frame* f, uint32_t* args) {
+  char* file_name = (char*)args[1];
+  return is_valid_file(file_name);
+}
+
+static bool validate_write(struct intr_frame* f, uint32_t* args) {
+  int fd = args[1];
+  void* buffer = (void*)args[2];
+  unsigned size = args[3];
+  return fd >= 0 && is_valid_buffer(buffer, size);
+}
+
+/* System call handlers */
+static void sys_halt_handler(struct intr_frame* f UNUSED, uint32_t* args UNUSED) {
+  shutdown_power_off(); // Shutdown the OS
+}
+
+static void sys_practice_handler(struct intr_frame* f, uint32_t* args) {
+  uint32_t value = args[1]; // Retrieve the first argument
+  f->eax = value + 1;       // Increment the value and store it in eax
+}
+
+static void sys_exit_handler(struct intr_frame* f, uint32_t* args) { terminate(f, args[1]); }
+
+static void sys_exec_handler(struct intr_frame* f, uint32_t* args) {
+  char* file_name = (char*)args[1];
+  pid_t pid = process_execute(file_name);
+  f->eax = (pid != TID_ERROR) ? pid : -1;
+}
+
+static void sys_write_handler(struct intr_frame* f, uint32_t* args) {
+  int fd = args[1];
+  const void* buffer = (void*)args[2];
+  unsigned size = args[3];
+  if (fd == STDOUT_FILENO) {
+    putbuf(buffer, size);
+    f->eax = size;
+  } else {
+    f->eax = -1; // Not implemented yet
+  }
+}
+
+/* Arrays for syscall validators and handlers */
+static validate_func syscall_validators[SYS_CALL_COUNT] = {
+    [SYS_HALT] = validate_halt,   [SYS_EXIT] = validate_exit,         [SYS_EXEC] = validate_exec,
+    [SYS_WRITE] = validate_write, [SYS_PRACTICE] = validate_practice,
+};
+
+static handler_func syscall_handlers[SYS_CALL_COUNT] = {
+    [SYS_HALT] = sys_halt_handler,         [SYS_EXIT] = sys_exit_handler,
+    [SYS_EXEC] = sys_exec_handler,         [SYS_WRITE] = sys_write_handler,
+    [SYS_PRACTICE] = sys_practice_handler,
+};
+
+/* Main syscall handler */
 static void syscall_handler(struct intr_frame* f) {
-  uint32_t* args = ((uint32_t*)f->esp);
+  uint32_t* args = (uint32_t*)f->esp;
 
-  if (!validate_pointer(args, sizeof(uint32_t))) {
+  /* Validate syscall number */
+  if (!is_valid_pointer(args, sizeof(uint32_t))) {
     terminate(f, -1);
-  };
+    return;
+  }
 
-  /* syscal_number will always be here */
   uint32_t syscall_number = args[0];
-
-  uint32_t arg_count = get_arg_count(syscall_number);
-
-  if (arg_count == -1) {
+  if (syscall_number >= SYS_CALL_COUNT || syscall_handlers[syscall_number] == NULL) {
     terminate(f, -1);
+    return;
   }
 
-  if (!validate_pointer(args, (arg_count + 1) * (sizeof(uint32_t)))) {
+  /* Validate syscall arguments */
+  if (syscall_validators[syscall_number] != NULL && !syscall_validators[syscall_number](f, args)) {
     terminate(f, -1);
-  };
-
-  /*
-   * The following print statement, if uncommented, will print out the syscall
-   * number whenever a process enters a system call. You might find it useful
-   * when debugging. It will cause tests to fail, however, so you should not
-   * include it in your final submission.
-   */
-
-  /* printf("System call number: %d\n", args[0]); */
-
-  if (syscall_number == SYS_EXIT) {
-    terminate(f, args[1]);
+    return;
   }
 
-  if (syscall_number == SYS_PRACTICE) {
-    f->eax = (int)args[1] + 1;
-  }
-
-  if (syscall_number == SYS_WRITE) {
-    int fd = args[1];
-    const void* buffer = (void*)args[2];
-    unsigned size = args[3];
-
-    if (fd == STDOUT_FILENO) {
-      putbuf(buffer, size);
-      f->eax = size; // Return the number of bytes written
-    } else {
-      // Handle other file descriptors (e.g., actual files)
-      f->eax = -1; // Not implemented yet
-    }
-  }
-
-  if (syscall_number == SYS_HALT) {
-    shutdown_power_off();
-  }
-
-  if (syscall_number == SYS_EXEC) {
-    char* file = args[1];
-    if (!validate_file(file)) {
-      terminate(f, -1);
-    };
-    // process_execute(file);
-  }
+  /* Execute the syscall */
+  syscall_handlers[syscall_number](f, args);
 }
